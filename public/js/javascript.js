092f3e14fe46ffa364789af98c4e043be8c67ad2
Gabriel fernandes <Gabrielfernandesfotografias@outlook.com>
2021-07-21 20:46:03 -0300

    manipulando a DOM

--- public/js/javascript.js
+++ public/js/javascript.js
@@ -6,6 +6,72 @@ console.log("oi, estou aqui")
 //<input type="email" class="form-control" id="inputEmail4">
 //</div>
 //
-//
+// <h1 class="title-page">Entendendo a árvore</h1>
+//<p id="paragrafo">
+//Quando criamos um repositório Git, criamos também uma árvore (tree). O código do projeto
+//inicial é um branch (galho), e a partir dele é possível criar novos branches, cada branch criado é uma cópia exata do seu branch pai no momento da criação. A criação de novos branches normalmente ocorre quando uma nova alteração no código precisa ser realizada. De modo que as alterações que o desenvolvedor realizar em uma branch serão salvas apenas nesta branch.
 
+//</p>
+
+// como buscar e pegar elemento na DOM
+
+// document.getElementById('');
+
+// const elemento = document.getElementById('paragrafo')
+
+// console.log(elemento)
+
+//   document.getElementById (Element)
+//   document.getElementsByClassName (HTMLCollection)
+//   document.getElementsByTagName (HTMLCollection)
+//   document.querySelector (Element)
+//   document.querySelectorAll (NodeList)
+
+
+//
+//  const element = document.querySelector("p")
+
+//  element.textContent = "ola, olha eu aqui no content" // aqui ele modifica
+
+//  element.textContent += " ola, olha eu aqui no content" // aqui ele add 
+
+//const element = document.querySelector('h1')
+//element.innerText = "innertext"
+//element.textContent += " - ola, olha eu aqui no content"
+
+//element.innerHTML += "<br> <div>vamos começar com uma div</div>"
+
+//const main = document.querySelector('main')
+//main.setAttribute('id', 'rg')
+
+//const mainID = document.querySelector('#rg')
+//console.log(mainID)
+
+////const main = document.querySelector('main')
+////main.setAttribute('id', 'rg') // add um atributo
+
+//console.log(main.getAttribute('id')) // mostrei o atributo
+
+//main.removeAttribute('id') // removi o atributo
+
+
+
+const elementbody = document.querySelector('body')
+elementbody.style.backgroundColor = "#f9f3d2"
+
+//const element = document.querySelector('body')
+
+//element.classList.add('active')
+
+// console.log(element.classList)
+
+//element.classList.toggle('active') // toggle se tiver active ele remove se nao tiver ele add
+
+//const h1 = document.querySelector('h1');
+
+//h1.addEventListener('click', print) // print é so um variavel, poderia ser qualquer nome
+
+//function print() {
+//    console.log('print')  
+//}
 
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,91 +2,9 @@
 @section('title', 'welcome')
 
 @section('content')
-    <h1>Entendendo a árvore</h1>
-    <p>
-        Quando criamos um repositório Git, criamos também uma árvore (tree). O código do projeto
-        inicial é um branch (galho), e a partir dele é possível criar novos branches, cada branch criado é uma cópia exata do seu branch pai no momento da criação. A criação de novos branches normalmente ocorre quando uma nova alteração no código precisa ser realizada. De modo que as alterações que o desenvolvedor realizar em uma branch serão salvas apenas nesta branch.
-        
-    </p>
-    
-    <form class="row gy-2 gx-3 align-items-center">
-        <div class="col-auto">
-          <label class="visually-hidden" for="autoSizingInput">Name</label>
-          <input type="text" class="form-control" id="autoSizingInput" placeholder="Jane Doe">
-        </div>
-        <div class="col-auto">
-          <label class="visually-hidden" for="autoSizingInputGroup">Username</label>
-          <div class="input-group">
-            <div class="input-group-text">@</div>
-            <input type="text" class="form-control" id="autoSizingInputGroup" placeholder="Username">
-          </div>
-        </div>
-        <div class="col-auto">
-          <label class="visually-hidden" for="autoSizingSelect">Preference</label>
-          <select class="form-select" id="autoSizingSelect">
-            <option selected>Choose...</option>
-            <option value="1">One</option>
-            <option value="2">Two</option>
-            <option value="3">Three</option>
-          </select>
-        </div>
-        <div class="col-auto">
-          <div class="form-check">
-            <input class="form-check-input" type="checkbox" id="autoSizingCheck">
-            <label class="form-check-label" for="autoSizingCheck">
-              Remember me
-            </label>
-          </div>
-        </div>
-        <div class="col-auto">
-          <button type="submit" class="btn btn-primary">Submit</button>
-        </div>
-      </form>
-      <form class="row g-3">
-        <div class="col-md-6">
-          <label for="inputEmail4" class="form-label">Email</label>
-          <input type="email" class="form-control" id="inputEmail4">
-        </div>
-        <div class="col-md-6">
-          <label for="inputPassword4" class="form-label">Password</label>
-          <input type="password" class="form-control" id="inputPassword4">
-        </div>
-        <div class="col-12">
-          <label for="inputAddress" class="form-label">Address</label>
-          <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
-        </div>
-        <div class="col-12">
-          <label for="inputAddress2" class="form-label">Address 2</label>
-          <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
-        </div>
-        <div class="col-md-6">
-          <label for="inputCity" class="form-label">City</label>
-          <input type="text" class="form-control" id="inputCity">
-        </div>
-        <div class="col-md-4">
-          <label for="inputState" class="form-label">State</label>
-          <select id="inputState" class="form-select">
-            <option selected>Choose...</option>
-            <option>...</option>
-          </select>
-        </div>
-        <div class="col-md-2">
-          <label for="inputZip" class="form-label">Zip</label>
-          <input type="text" class="form-control" id="inputZip">
-        </div>
-        <div class="col-12">
-          <div class="form-check">
-            <input class="form-check-input" type="checkbox" id="gridCheck">
-            <label class="form-check-label" for="gridCheck">
-              Check me out
-            </label>
-          </div>
-        </div>
-        <div class="col-12">
-          <button type="submit" class="btn btn-primary">Sign in</button>
-        </div>
 
 
 
-    
-@endsection
+
+ @endsection
+      
